02fbade76975803ea8c45bb1a070db039c470e68
Documenter.jl <documenter@juliadocs.github.io>
2024-09-29 10:55:15 +0000

    build based on 954e702

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KhepriIllustrator","category":"page"},{"location":"#KhepriIllustrator","page":"Home","title":"KhepriIllustrator","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KhepriIllustrator.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KhepriIllustrator]","category":"page"}]
+}
